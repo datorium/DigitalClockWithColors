c87bea7d00e14575d73d5aeb7fddf12832f4038e
Datorium <hello@datorium.lv>
2020-10-17 11:51:10 +0300

    version 11:51

--- Form1.cs
+++ Form1.cs
@@ -25,7 +25,27 @@ namespace DigitalClockWithColors
 
         private void MainTimer_Tick(object sender, EventArgs e)
         {
+            dsec += 1;
+            if(dsec == 10)
+            {
+                sec += 1;
+                dsec = 0;
+            }
+            if(sec == 60)
+            {
+                sec = 0;
+            }
+            UpdateClockDisplay();
+        }
 
+        private void UpdateClockDisplay()
+        {
+            string timeText;
+            timeText = hou.ToString("00") + ":";
+            timeText += min.ToString("00") + ":";
+            timeText += sec.ToString("00") + ":";
+            timeText += dsec.ToString("0");
+            ClockDisplay.Text = timeText;
         }
     }
 }
